2c78a449b06e057ede36cc2c6ea1e77d0723758e
Stein Runar Bergheim <runar@bergheim.dk>
2015-09-18 13:24:47 +0200

    Added object to parse response from myabudhabi.net to

---
+++ norplan.adm.qrlib/MyAbuDhabiNetCodesResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace norplan.adm.qrlib
+{
+    public class MyAbuDhabiNetCodesResponse
+    {
+        [JsonProperty(PropertyName = "time")]
+        public double Time { get; set; }
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
+        [JsonProperty(PropertyName = "codes")]
+        public List<string> Codes { get; set; }
+    }
+}
